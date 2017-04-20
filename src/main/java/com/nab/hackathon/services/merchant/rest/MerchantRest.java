658d03ded8a64aac13ede528312c24e0e0b3877d
Camilo Rivera <Camilo.AR.Damelines@nab.com.au>
2017-04-20 16:57:23 +1000

    More rest methods for merchant

--- src/main/java/com/nab/hackathon/services/merchant/rest/MerchantRest.java
+++ src/main/java/com/nab/hackathon/services/merchant/rest/MerchantRest.java
@@ -1,32 +1,45 @@
 package com.nab.hackathon.services.merchant.rest;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.nab.hackathon.services.merchant.entity.Merchant;
+import com.nab.hackathon.services.merchant.repository.MerchantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
-import com.nab.hackathon.services.merchant.repository.MerchantRepository;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/merchant")
-public class MerchantRest
-{
-    private static Log logger = LogFactory.getLog(com.nab.hackathon.services.merchant.rest.MerchantRest.class);
+public class MerchantRest {
 
-    @Autowired
-    private MerchantRepository merchantRepository;
+  @Autowired
+  private MerchantRepository merchantRepository;
 
-    @RequestMapping(value = "/{merchantId}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<com.nab.hackathon.services.merchant.entity.Merchant> getMerchant()
-    {
-        logger.info("Invoking /merchant/all RESTful method");
-        return merchantRepository.findAll();
-    }
+  @RequestMapping(value = "all", method = GET, produces = APPLICATION_JSON_VALUE)
+  public List<Merchant> allMerchants() {
+    return merchantRepository.findAll();
+  }
+
+  @RequestMapping(value = "/{merchantId}", method = GET, produces = APPLICATION_JSON_VALUE)
+  public Merchant findByMerchantId(@PathParam("merchantId") Long merchantId) {
+    return merchantRepository.findOne(merchantId);
+  }
+
+  @RequestMapping(method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+  public Merchant create(Merchant merchant) {
+    merchantRepository.save(merchant);
+    return merchant;
+  }
+
+  @RequestMapping(value = "all", method = DELETE)
+  public void delete(Long merchantId) {
+    merchantRepository.delete(merchantId);
+  }
+
 }
